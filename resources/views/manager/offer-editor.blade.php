d4b865f08d897c9e70b8b6b189b5956e8843499d
mrsbaai <abdelilah.sbaai@gmail.com>
2018-08-15 20:17:19 +0100

    fix edit offer

--- resources/views/manager/offer-editor.blade.php
+++ resources/views/manager/offer-editor.blade.php
@@ -33,6 +33,7 @@
             <div class="row">
                 <div class="col-md-4">
                     <label for="verticals">Niches:</label>
+                    @if( empty($selected_verticals)) {{$selected_verticals = null}} @endif
                     {!! Form::select('verticals[]', $verticals, $selected_verticals, ['multiple' => true, 'class' => 'form-control long']) !!}
 
                 </div>
