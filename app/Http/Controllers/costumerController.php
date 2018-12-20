c5103a857a2c498d60cf19ddf9fa2afab95d5ee5
Desk <abdelilah.sbaai@gmail.com>
2018-12-20 17:38:49 +0100

    mn*

--- app/Http/Controllers/costumerController.php
+++ app/Http/Controllers/costumerController.php
@@ -24,6 +24,7 @@ class costumerController extends Controller
 
 
         $files = Storage::disk('dropbox')->allFiles($id);
+
         return Storage::disk('dropbox')->download($files[0]);
     }
     public function home(request $request){
@@ -60,13 +61,14 @@ class costumerController extends Controller
         $table->addColumn('price')
             ->setTitle('')
             ->isCustomHtmlElement(function ($entity, $column) {
-
+                $files = Storage::disk('dropbox')->allFiles($entity->offer_id);
+                $download_url = Storage::disk('dropbox')->download($files[0]);
 
 
                 if ($entity->paid == true){
-                    return '<a href="' . $download_url . '" class="btn btn-success float-right">Download</a>';
+                    return '<a href="' .  asset($files[0]) . '" class="btn btn-success float-right">Download</a>';
                 }else{
-                    return '<a style="margin-left: 13px;"href="/download/' .  $entity->offer_id . '" class="btn btn-danger float-right">Cancel</a>'
+                    return '<a style="margin-left: 13px;"href="' . $download_url . '" class="btn btn-danger float-right">Cancel</a>'
                         . '<a href="/" class="btn btn-info  float-right">Pay Now (Only $' . $entity->price . ')</a>';
                 }
             });
