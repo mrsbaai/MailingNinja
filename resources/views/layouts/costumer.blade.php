2ae73d92560583e79641e3d4b571d785522dc268
Desk <abdelilah.sbaai@gmail.com>
2018-12-20 19:43:43 +0100

    mn*$

--- resources/views/layouts/costumer.blade.php
+++ resources/views/layouts/costumer.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>Mailing.Ninja :: @yield('title')</title>
+    <title>{{strtoupper(explode(".", Request::getHost())[0])}}</title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
