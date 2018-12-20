ac53c31a24726208263f2d7d26bb2072575b91d0
Desk <abdelilah.sbaai@gmail.com>
2018-12-20 19:28:38 +0100

    mn*$

--- resources/views/layouts/costumer.blade.php
+++ resources/views/layouts/costumer.blade.php
@@ -33,11 +33,7 @@
 
                     <span class="navbar-brand">
 
-                        <span class="uppercase">
-
-                                {{explode(".", Request::getHost())[0]}}
-
-                        </span>
+                        <span class="uppercase">{{explode(".", Request::getHost())[0]}}</span><span id="logo_span" style="font-size:200%;color:#7cc576;">.</span>
                     </span>
 
 
@@ -105,7 +101,7 @@
                 ©
                 <script>
                   document.write(new Date().getFullYear())
-                </script>, Made with <i class="fa fa-heart heart"></i> by <span class="uppercase">{{explode(".", Request::getHost())[0]}}</span>
+                </script>, Made with <i class="fa fa-heart heart"></i> by <span class="uppercase">{{explode(".", Request::getHost())[0]}}</span><span id="logo_span" style="font-size:200%;color:#7cc576;">.</span>
               </span>
                     </div>
                 </div>
