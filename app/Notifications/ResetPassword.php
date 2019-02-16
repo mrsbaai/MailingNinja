5cd692ef30f070b812dac2da4ab4101c0b43a8f7
mrsbaai <abdelilah.sbaai@gmail.com>
2019-02-16 18:46:02 +0100

    maillable

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -54,7 +54,7 @@ class ResetPassword extends ResetPasswordNotification
 
         $user = User::all()->where("remember_token",$this->token)->first();
 
-        $roles = $user->roles()->get();
+        $ispublisher = $user->hasRole("publisher");
 
         Config::set('app.name', config('app.home_name'));
         Config::set('app.url', config('app.home_url'));
@@ -65,7 +65,7 @@ class ResetPassword extends ResetPasswordNotification
         return (new MailMessage)
             ->from($from_e,$from_n)
             ->subject('Reset Password')
-            ->line($roles . 'You are receiving this email because we received a password reset request for your account.')
+            ->line($ispublisher . 'You are receiving this email because we received a password reset request for your account.')
             ->action('Reset Password', $ur.route('password.reset', $this->token, false))
             ->line('If you did not request a password reset, no further action is required.');
 
