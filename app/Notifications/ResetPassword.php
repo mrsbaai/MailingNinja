44b39e67f8fddafa0cd5a215730f650a23679c18
mrsbaai <abdelilah.sbaai@gmail.com>
2019-02-16 18:49:27 +0100

    maillable

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -65,7 +65,7 @@ class ResetPassword extends ResetPasswordNotification
         return (new MailMessage)
             ->from($from_e,$from_n)
             ->subject('Reset Password')
-            ->line($user(['id']) . 'You are receiving this email because we received a password reset request for your account.')
+            ->line($user(['name']) . 'You are receiving this email because we received a password reset request for your account.')
             ->action('Reset Password', $ur.route('password.reset', $this->token, false))
             ->line('If you did not request a password reset, no further action is required.');
 
