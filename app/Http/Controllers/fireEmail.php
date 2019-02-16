64e528eb8951797f617f6fa5f645efa68df7fb09
mrsbaai <abdelilah.sbaai@gmail.com>
2019-02-16 15:55:01 +0100

    maillable

--- app/Http/Controllers/fireEmail.php
+++ app/Http/Controllers/fireEmail.php
@@ -25,6 +25,7 @@ class fireEmail extends Controller
         app()->forgetInstance('swift.mailer');
         app()->forgetInstance('mailer');
         Mail::clearResolvedInstance('mailer');
+        return "bb";
         if ($is_costumer == true){
             Config::set('services.mailgun.domain', config('app.mailgun_domain_costumer'));
             Config::set('services.mail.username', config('app.mail_username_costumer'));
