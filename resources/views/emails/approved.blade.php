dd43a5681c8ffea617300f9be952585adac1e738
mrsbaai <abdelilah.sbaai@gmail.com>
2019-02-19 15:01:18 +0100

    costumer registred

--- resources/views/emails/approved.blade.php
+++ resources/views/emails/approved.blade.php
@@ -10,13 +10,16 @@ Please use the login credentials you created upon registration to sign in to you
 @endcomponent
 
 E-mail: {{$data['email']}}
+
 Password: Created at signup.
 
 If you forgot your password, simply hit "Forgot password" and you'll be prompted to reset it.
 
 #Please complete your account info before using the offers.
 
-Your <b>dedicated account manager</b> is: "{{$data['manager_name']}}". Contact your account manager via e-mail at "{{$data['manager_email']}}", or via Skype at "{{$data['manager_skype']}}".
+<b>Your dedicated account manager name is: "{{$data['manager_name']}}".</b>
+
+Contact your account manager via e-mail at "{{$data['manager_email']}}", or via Skype at "{{$data['manager_skype']}}".
 
 Your account manager and our knowledgeable support team are available 7 days/week. Please contact us with any questions you might have. We can also help you select campaigns that will work well with your preferred promotion styles.
 
