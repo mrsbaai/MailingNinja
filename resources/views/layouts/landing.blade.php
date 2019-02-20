4907a9e1486decac5d1badd5f66d6ff8fc1afe36
Desk <abdelilah.sbaai@gmail.com>
2019-02-20 23:39:20 +0100

    confirm

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -147,8 +147,7 @@
     <link id="main" rel="stylesheet" type="text/css" href="{{ asset('landing/css/publisher.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('landing/css/book.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('landing/css/subscribe.css') }}">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-
+   
 </head>
 
 
