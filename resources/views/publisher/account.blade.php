93f4f7cc8b8f41e3ed9bd6301988f56fa770a587
Desk <abdelilah.sbaai@gmail.com>
2019-02-21 20:20:05 +0100

    padding

--- resources/views/publisher/account.blade.php
+++ resources/views/publisher/account.blade.php
@@ -137,7 +137,7 @@
                                         <div class="help-block with-errors"></div>
                                     </div>
                                 </div>
-                                <p style="padding-left: 20x;"><b>Important!</b> PayPal Email is required to receive payments from costumers.</p>
+                                <p><b>&nbsp;&nbsp;Important!</b> PayPal Email is required to receive payments from costumers.</p>
 
                             </div>
 
