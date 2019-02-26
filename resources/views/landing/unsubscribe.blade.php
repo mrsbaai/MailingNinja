5cf74d553b9d1e429a4998a14344e390c57bfc17
Desk <abdelilah.sbaai@gmail.com>
2019-02-27 00:08:09 +0100

    fix bug

--- resources/views/landing/unsubscribe.blade.php
+++ resources/views/landing/unsubscribe.blade.php
@@ -15,14 +15,16 @@
 
             {{ Form::open(array('action' => 'subscribeController@unsubscribe'))}}
             <div class = "col-12">
-                <div class="row">
-                    <h2 class="title">Do you want to unsubscribe?</h2>
+                <div><h2 class="title">Do you want to unsubscribe?</h2></div>
+                <div> <input name="email" type="text" style="max-width: 400px;" class="form-control" placeholder="Email to unsubscribe"></div>
+                <div> <input class="btn btn-danger"  type="submit" value="Unsubscribe"></div>
 
-                    <input name="email" type="text" style="max-width: 400px;" class="form-control" placeholder="Email to unsubscribe">
 
-                    <input class="btn btn-danger"  type="submit" value="Unsubscribe">
 
-                </div>
+
+
+
+
 
             </div>
 
