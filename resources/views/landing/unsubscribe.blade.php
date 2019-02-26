9637e926fc89ed93f26e239d69a662a83dde88c6
Desk <abdelilah.sbaai@gmail.com>
2019-02-26 23:33:54 +0100

    fix bug

--- resources/views/landing/unsubscribe.blade.php
+++ resources/views/landing/unsubscribe.blade.php
@@ -1,8 +1,3 @@
-@extends('layouts.home')
-
-
-@section('content')
-
 
 
     <h1 class="heading uppercase" style="margin-top: -70px; font-size: 500%">{{ config('app.name') }}<span id="logo_span" style="font-size:200%;color:#7cc576;">.</span></h1>
@@ -17,7 +12,7 @@
                 {{ Form::open(array('action' => 'subscribeController@subscribePost'))}}
                     <div><h2 class="title">Do you want to unsubscribe?</h2></div>
                     <div><label class="title">Email to unsubscribe:</label><input autocomplete="on" type="text" name="email" value=""></div>
-   
+
                     <div><input type="submit" name="unsubscribe" value="Unsubscribe"></div>
 
             {{ Form::open(array('action' => 'subscribeController@subscribePost'))}}
@@ -27,6 +22,3 @@
 
 
 
-
-
-@endsection
