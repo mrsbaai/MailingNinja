246704d8eda45ca842ed28fe1319476089015b63
Desk <abdelilah.sbaai@gmail.com>
2019-02-26 14:02:03 +0100

    fix bug

--- resources/views/emails/costumerSubscribed.blade.php
+++ resources/views/emails/costumerSubscribed.blade.php
@@ -16,6 +16,7 @@ Or copy and paste this link in a browser:
 
 {{"https://" . config('app.url') . "/confirm/" . $data['email']}}
 
+*You can <a href="{{"https://" . config('app.url') . "/unsubscribe/" . $data['email']}}">Unsubscribe</a> from marketing emails at any time.
 
 
 If you received this email by mistake, simply delete it. You won't be subscribed if you don't click the confirmation link above.
