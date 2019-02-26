849df6b7e96eeca603b2a41005b9bbee23692567
Desk <abdelilah.sbaai@gmail.com>
2019-02-26 23:42:05 +0100

    fix bug

--- resources/views/layouts/costumer.blade.php
+++ resources/views/layouts/costumer.blade.php
@@ -40,6 +40,7 @@
 
 
                 </div>
+                @if(Auth::check())
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-bar navbar-kebab"></span>
                     <span class="navbar-toggler-bar navbar-kebab"></span>
@@ -72,6 +73,7 @@
                         </li>
                     </ul>
                 </div>
+                    @endif
             </div>
         </nav>
         <!-- End Navbar -->
