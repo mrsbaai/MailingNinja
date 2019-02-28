eb3d2f0f39c090dd4a402eed4379010f59e22cab
Desk <abdelilah.sbaai@gmail.com>
2019-02-28 22:54:16 +0100

    up

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -168,7 +168,7 @@
         <div class="container" >
 
             <div class="col-md-6 intro-text col-sm-12  col-xs-12 @if($cover == null or $cover == "") hidden-sm hidden-xs @endif">
-                <img class="img-responsive img-fluid  pl-sm-3 pl-xs-3 pr-sm-3 pr-xs-3" style=" display:block;margin:auto; max-height: 400px;" src="@if($cover != null and $cover != ""){{ $cover }}@else {{ $thumbnail }} @endif" />
+                <img class="img-responsive img-fluid  pl-sm-3 pl-xs-3 pr-sm-3 pr-xs-3" style=" display:block;margin:auto; max-height: 500px;" src="@if($cover != null and $cover != ""){{ $cover }}@else {{ $thumbnail }} @endif" />
             </div>
 
 
