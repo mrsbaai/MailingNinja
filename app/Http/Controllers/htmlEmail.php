a08bd2fb8a4d228391c9d6d2de375b9e77c82d59
Desk <abdelilah.sbaai@gmail.com>
2019-03-01 07:04:26 +0100

    up

--- app/Http/Controllers/htmlEmail.php
+++ app/Http/Controllers/htmlEmail.php
@@ -71,26 +71,22 @@ class htmlEmail extends Controller
 
     public function screenshot(){
         $client = Client::getInstance();
+        $client->getEngine()->setPath('../bin/phantomjs.exe');
 
-        $width  = 550;
-        $height = 150;
-        $top    = 0;
-        $left   = 0;
 
-        /**
-         * @see JonnyW\PhantomJs\Http\CaptureRequest
-         **/
-        $request = $client->getMessageFactory()->createCaptureRequest('https://mailing.ninja/preview/email/YKBLS/unsubscribe', 'GET');
-        $request->setOutputFile('/file.jpg');
-        $request->setViewportSize($width, $height);
-        $request->setCaptureDimensions($width, $height, $top, $left);
+        $request = $client->getMessageFactory()->createRequest('http://jonnyw.me', 'GET');
 
-        /**
-         * @see JonnyW\PhantomJs\Http\Response
-         **/
+
         $response = $client->getMessageFactory()->createResponse();
 
         // Send the request
         $client->send($request, $response);
+
+        if($response->getStatus() === 200) {
+
+            // Dump the requested page content
+            echo $response->getContent();
+        }
+
     }
 }
