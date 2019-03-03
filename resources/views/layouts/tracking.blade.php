3a8472149b649f5c5af8b7d05e7217c6e5d2d52e
mrsbaai <abdelilah.sbaai@gmail.com>
2019-03-03 02:59:27 +0100

    fix tracking

--- resources/views/layouts/tracking.blade.php
+++ resources/views/layouts/tracking.blade.php
@@ -2,7 +2,7 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-135440157-1"></script>
 <script>
-    _gaq.push(['{{config("promote_url")}}', 'mailing.ninja']);
+
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
