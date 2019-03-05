3a9a4edb7d07bcfe1831fa6c41bcc58786cf4b6c
mrsbaai <abdelilah.sbaai@gmail.com>
2019-03-05 19:12:56 +0100

    re logo

--- resources/views/mailingNinja/home.blade.php
+++ resources/views/mailingNinja/home.blade.php
@@ -5,7 +5,6 @@
         <div class="filter"></div>
         <div class="container">
             <div class="motto text-center">
-                <img srce="/images/logo.png"/>
                 <h1>Mailing Ninja</h1>
                 <h3>Exclusive Offers For <b><u>Traffic Monetization</u></b> & <b><u>E-Mail List Building</u></b></h3>
                 <br />
