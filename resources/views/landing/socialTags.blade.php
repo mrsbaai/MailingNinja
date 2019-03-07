b83fd98ae2e4282ed2f5e50e63cea80ac1fbe961
mrsbaai <abdelilah.sbaai@gmail.com>
2019-03-07 18:45:44 +0100

    added social ags

--- resources/views/landing/socialTags.blade.php
+++ resources/views/landing/socialTags.blade.php
@@ -4,20 +4,20 @@
     <meta charset="utf-8">
     <title>[Download] {{$title}}</title>
     <!-- Search Engine -->
-    <meta name="description" content="{{$subtitle}}\n: {{$description}}">
+    <meta name="description" content="{{$subtitle}}\r\n\r\n: {{$description}}">
     <meta name="image" content="{{$thumbnail}}">
     <!-- Schema.org for Google -->
     <meta itemprop="name" content="[Download] {{$title}}">
-    <meta itemprop="description" content="{{$subtitle}}\n: {{$description}}">
+    <meta itemprop="description" content="{{$subtitle}}\r\n\r\n: {{$description}}">
     <meta itemprop="image" content="{{$thumbnail}}">
     <!-- Twitter -->
     <meta name="twitter:card" content="summary">
     <meta name="twitter:title" content="[Download] {{$title}}">
-    <meta name="twitter:description" content="{{$subtitle}}\n: {{$description}}">
+    <meta name="twitter:description" content="{{$subtitle}}\r\n\r\n: {{$description}}">
     <meta name="twitter:image:src" content="{{$thumbnail}}">
     <!-- Open Graph general (Facebook, Pinterest & Google+) -->
     <meta name="og:title" content="[Download] {{$title}}">
-    <meta name="og:description" content="{{$subtitle}}\n: {{$description}}">
+    <meta name="og:description" content="{{$subtitle}}\r\n\r\n: {{$description}}">
     <meta name="og:image" content="{{$thumbnail}}">
     <meta name="og:url" content="https://{{ config('app.promote_url') }}/{{$code}}">
     <meta name="og:site_name" content="config('app.promote_url')">
