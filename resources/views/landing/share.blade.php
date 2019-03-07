3bf17445ee5e583d678602c534ceebef9e690597
mrsbaai <abdelilah.sbaai@gmail.com>
2019-03-07 19:07:16 +0100

    added social ags

---
+++ resources/views/landing/share.blade.php
@@ -0,0 +1,7 @@
+
+@section('share')
+
+@endsection
+@section('shareJS')
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4e68c2194e3aa462"></script>
+@endsection
