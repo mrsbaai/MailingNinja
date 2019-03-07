21a8b94b2282670dced053902074fdade3a4bef3
mrsbaai <abdelilah.sbaai@gmail.com>
2019-03-07 19:27:36 +0100

    added social ags

--- resources/views/landing/subscribe.blade.php
+++ resources/views/landing/subscribe.blade.php
@@ -21,6 +21,19 @@
 
     <h4><span class="label label-info"><i class="fas fa-envelope fa-md"></i> {{$subscribers_count}} Subscribed</span></h4>
     <br/>
+    <!-- AddToAny BEGIN -->
+    <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
+        <a class="a2a_dd" href="https://www.addtoany.com/share"></a>
+        <a class="a2a_button_facebook"></a>
+        <a class="a2a_button_twitter"></a>
+        <a class="a2a_button_google_plus"></a>
+        <a class="a2a_button_whatsapp"></a>
+        <a class="a2a_button_email"></a>
+        <a class="a2a_button_pinterest"></a>
+    </div>
+    <script async src="https://static.addtoany.com/menu/page.js"></script>
+    <!-- AddToAny END -->
+    
 
     <script>
 
