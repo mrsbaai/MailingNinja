72bac76be82990df9d619f4912fc8c93fb998b7c
mrsbaai <abdelilah.sbaai@gmail.com>
2019-03-07 19:25:21 +0100

    added social ags

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -603,9 +603,19 @@
     });
 
 
-</script><!-- Go to www.addthis.com/dashboard to customize your tools -->
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4e68c2194e3aa462"></script>
-
+</script>
+<!-- AddToAny BEGIN -->
+<div class="a2a_kit a2a_kit_size_32 a2a_default_style">
+    <a class="a2a_dd" href="https://www.addtoany.com/share"></a>
+    <a class="a2a_button_facebook"></a>
+    <a class="a2a_button_twitter"></a>
+    <a class="a2a_button_google_plus"></a>
+    <a class="a2a_button_whatsapp"></a>
+    <a class="a2a_button_email"></a>
+    <a class="a2a_button_pinterest"></a>
+</div>
+<script async src="https://static.addtoany.com/menu/page.js"></script>
+<!-- AddToAny END -->
 </body>
 
 </html>
