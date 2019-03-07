516a6bf8bd25c2f97937cba376479d21faa59f4f
mrsbaai <abdelilah.sbaai@gmail.com>
2019-03-07 19:41:05 +0100

    added social ags

--- resources/views/landing/subscribe.blade.php
+++ resources/views/landing/subscribe.blade.php
@@ -20,18 +20,7 @@
     {{ Form::close()}}
 
     <h4><span class="label label-info"><i class="fas fa-envelope fa-md"></i> {{$subscribers_count}} Subscribed</span></h4>
-    <br/>
-    <!-- AddToAny BEGIN -->
-    <div>
-        <a href="https://www.addtoany.com/share#url={{url()->current()}}&amp;title=" target="_blank"><img src="https://static.addtoany.com/buttons/a2a.svg" width="32" height="32" style="background-color:black"></a>
-        <a href="https://www.addtoany.com/add_to/facebook?linkurl={{url()->current()}}&amp;linkname=" target="_blank"><img src="https://static.addtoany.com/buttons/facebook.svg" width="32" height="32" style="background-color:black"></a>
-        <a href="https://www.addtoany.com/add_to/twitter?linkurl={{url()->current()}}&amp;linkname=" target="_blank"><img src="https://static.addtoany.com/buttons/twitter.svg" width="32" height="32" style="background-color:black"></a>
-        <a href="https://www.addtoany.com/add_to/google_plus?linkurl={{url()->current()}}&amp;linkname=" target="_blank"><img src="https://static.addtoany.com/buttons/google_plus.svg" width="32" height="32" style="background-color:black"></a>
-        <a href="https://www.addtoany.com/add_to/pinterest?linkurl={{url()->current()}}&amp;linkname=" target="_blank"><img src="https://static.addtoany.com/buttons/pinterest.svg" width="32" height="32" style="background-color:black"></a>
-        <a href="https://www.addtoany.com/add_to/whatsapp?linkurl={{url()->current()}}&amp;linkname=" target="_blank"><img src="https://static.addtoany.com/buttons/whatsapp.svg" width="32" height="32" style="background-color:black"></a>
-        <a href="https://www.addtoany.com/add_to/email?linkurl={{url()->current()}}&amp;linkname=" target="_blank"><img src="https://static.addtoany.com/buttons/email.svg" width="32" height="32" style="background-color:black"></a>
-    </div>
-    <!-- AddToAny END -->
+
     <br/>
 
 
--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -298,6 +298,19 @@
 
                             <p class="small justify muted-light wow animated fadeInUp">{{ $author_about }}.</p>
 
+                            <br/>
+                            <!-- AddToAny BEGIN -->
+                            <div class="text-center">
+                                <a href="https://www.addtoany.com/share#url={{url()->current()}}&amp;title=" target="_blank"><img src="https://static.addtoany.com/buttons/a2a.svg" width="32" height="32" style="background-color:black"></a>
+                                <a href="https://www.addtoany.com/add_to/facebook?linkurl={{url()->current()}}&amp;linkname=" target="_blank"><img src="https://static.addtoany.com/buttons/facebook.svg" width="32" height="32" style="background-color:black"></a>
+                                <a href="https://www.addtoany.com/add_to/twitter?linkurl={{url()->current()}}&amp;linkname=" target="_blank"><img src="https://static.addtoany.com/buttons/twitter.svg" width="32" height="32" style="background-color:black"></a>
+                                <a href="https://www.addtoany.com/add_to/google_plus?linkurl={{url()->current()}}&amp;linkname=" target="_blank"><img src="https://static.addtoany.com/buttons/google_plus.svg" width="32" height="32" style="background-color:black"></a>
+                                <a href="https://www.addtoany.com/add_to/pinterest?linkurl={{url()->current()}}&amp;linkname=" target="_blank"><img src="https://static.addtoany.com/buttons/pinterest.svg" width="32" height="32" style="background-color:black"></a>
+                                <a href="https://www.addtoany.com/add_to/whatsapp?linkurl={{url()->current()}}&amp;linkname=" target="_blank"><img src="https://static.addtoany.com/buttons/whatsapp.svg" width="32" height="32" style="background-color:black"></a>
+                                <a href="https://www.addtoany.com/add_to/email?linkurl={{url()->current()}}&amp;linkname=" target="_blank"><img src="https://static.addtoany.com/buttons/email.svg" width="32" height="32" style="background-color:black"></a>
+                            </div>
+                            <!-- AddToAny END -->
+
                         </div>
                     </div>
 
