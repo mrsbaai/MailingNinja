48b8cf3ed4f02aba127924dc1f67851b8c3c1df3
mrsbaai <abdelilah.sbaai@gmail.com>
2019-04-06 16:55:24 +0100

    cpa

--- app/Http/Controllers/publisherController.php
+++ app/Http/Controllers/publisherController.php
@@ -512,7 +512,42 @@ class publisherController extends Controller
 
             });
 
-       
+        $table->addColumn('TYPE')
+
+            ->setTitle('TYPE')
+            ->isCustomHtmlElement(function ($entity, $column) {
+                if ($entity->cpc > 0){
+                    Return "CPC";
+                }elseif ($entity->cpa > 0){
+                    Return "CPA";
+                }else{
+                    Return "-";
+                }
+
+            });
+
+        $table->addColumn('PAYOUT')
+            ->setTitle('PAYOUT')
+            ->isCustomHtmlElement(function ($entity, $column) {
+                if ($entity->cpc > 0){
+                    Return $entity->cpc;
+                }elseif ($entity->cpa > 0){
+                    Return $entity->cpa;
+                }else{
+                    Return "Direct";
+                }
+
+            });
+
+
+        $table->addColumn('created_at')
+            ->isSortable()
+            ->setTitle('DATE')
+            ->isCustomHtmlElement(function ($entity, $column) {
+                return $entity->created_at;
+
+            });
+
 if($request->user()->is_monetize == false){
     $table->addColumn('')
         ->setTitle(__('Subscribes'))
@@ -537,14 +572,11 @@ if($request->user()->is_monetize == false){
 
         $table->addColumn()
             ->setTitle(__(' '))
-            ->isCustomHtmlElement(function ($request, $entity, $column) {
+            ->isCustomHtmlElement(function ( $entity, $column) {
 
                 $preview_route = route('preview', ['id' => $entity->id]);
                 $promote_route = route('promote-offer', ['id' => $entity->id]);
                 $stats_route = route('offer-stats', ['id' => $entity->id]);
-                if($request->user()->is_monetize == false){
-                    $download_route = route('offer-subscribed', ['id' => $entity->id]);
-                }
 
 
                 $return =   "
@@ -552,9 +584,7 @@ if($request->user()->is_monetize == false){
 <a class='p-3' target='_blank' href='$stats_route'  title='Show Offer Statistics'><i class='fas fa-fw fa-chart-bar'></i></a>
 <a class='p-3' target='_blank' href='$promote_route'  title='Show Promotional Links & Tools'><i class='fas fa-fw fa-link'></i></a>";
 
-                if($request->user()->is_monetize == false){
-                    $return = $return . "<a class='p-3' target='_blank' href='$download_route'  title='Download Subscribed E-mail List'><i class='fas fa-fw fa-arrow-down'></i></a>";
-                }
+
                 ;
 
                 if ($entity->is_private == true){
