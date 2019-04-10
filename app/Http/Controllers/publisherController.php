6d1eb0ac529bc9d861abcc5d67ff431bc559717d
mrsbaai <abdelilah.sbaai@gmail.com>
2019-04-10 20:47:26 +0100

    cpc to dashboard

--- app/Http/Controllers/publisherController.php
+++ app/Http/Controllers/publisherController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\cpa;
 use App\cpc;
 use App\opens;
 use App\unsubscribes;
@@ -302,6 +303,10 @@ class publisherController extends Controller
         $cpcChart30= $this->cpcChart($user_id, null, $offer_id, 30);
         $cpcChart90 = $this->cpcChart($user_id, null, $offer_id, 90);
 
+        $cpaChart7 = $this->cpaChart($user_id, null, $offer_id, 7);
+        $cpaChart30= $this->cpaChart($user_id, null, $offer_id, 30);
+        $cpaChart90 = $this->cpaChart($user_id, null, $offer_id, 90);
+
         $OpenChart7 = $this->OpenChart($user_id, null, $offer_id, 7);
         $OpenChart30 = $this->OpenChart($user_id, null, $offer_id, 30);
         $OpenChart90 = $this->OpenChart($user_id, null, $offer_id, 90);
@@ -320,15 +325,13 @@ class publisherController extends Controller
             ->with('cpcChart7',$cpcChart7)
             ->with('cpcChart30',$cpcChart30)
             ->with('cpcChart90',$cpcChart90)
+            ->with('cpaChart7',$cpaChart7)
+            ->with('cpaChart30',$cpaChart30)
+            ->with('cpaChart90',$cpaChart90)
             ->with('OpenChart7',$OpenChart7)
             ->with('OpenChart30',$OpenChart30)
             ->with('OpenChart90',$OpenChart90)
-            ->with('ProfitChart7',$ProfitChart7)
-            ->with('ProfitChart30',$ProfitChart30)
-            ->with('ProfitChart90',$ProfitChart90)
-            ->with('LeadsChart7',$LeadsChart7)
-            ->with('LeadsChart30',$LeadsChart30)
-            ->with('LeadsChart90',$LeadsChart90)
+
             ->with('title', $title);
     }
 
@@ -955,6 +958,64 @@ if($request->user()->is_monetize == false){
         return $chartjs;
     }
 
+
+    public function cpaChart ($user_id = null, $vertical_id = null, $offer_id = null, $days = 30){
+
+        $name = substr(str_shuffle(str_repeat($x='ABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil(5/strlen($x)) )),1,5);
+        $start = Carbon::now()->subDays($days);
+
+        for ($i = 0 ; $i <= $days; $i++) {
+            $date = $start->copy()->addDays($i);
+            $dates[] = $date->format('d/m');
+
+            $query  = cpa::latest();
+
+
+            $query->whereMonth('created_at',$date->format('m'))
+                ->whereDay('created_at',$date->format('d'));
+
+            if ($user_id){$query->where('user_id' , $user_id);}
+            if ($offer_id){$query->where('offer_id' , $offer_id);}
+            $collection = $query->get();
+            $profit = 0;
+            foreach ($collection as $item) {
+                $profit = $profit + $item['count'];
+            }
+
+            $data[] = $profit;
+        }
+
+        $chartjs = app()->chartjs
+            ->name($name)
+            ->type('line')
+            ->size(['width' => 350, 'height' => 150])
+            ->labels($dates)
+            ->datasets([
+                [
+                    "label" => "Profit ($)",
+                    'backgroundColor' => "rgba(38, 185, 154, 0.31)",
+                    'borderColor' => "rgba(38, 185, 154, 0.7)",
+                    "pointBorderColor" => "rgba(38, 185, 154, 0.7)",
+                    "pointBackgroundColor" => "rgba(38, 185, 154, 0.7)",
+                    "pointHoverBackgroundColor" => "#fff",
+                    "pointHoverBorderColor" => "rgba(220,220,220,1)",
+                    'data' => $data,
+                ],
+            ])
+            ->options([
+                'scales' => [
+                    'yAxes' => [
+                        [
+                            'ticks' => [
+                                'beginAtZero' => true,
+                                'precision' => 0,
+                            ]
+                        ]
+                    ]
+                ]
+            ]);
+        return $chartjs;
+    }
 
     public function OpenChart ($user_id = null, $vertical_id = null, $offer_id = null, $days = 30){
 
--- resources/views/publisher/statistics.blade.php
+++ resources/views/publisher/statistics.blade.php
@@ -247,6 +247,56 @@
             </div>
          </div>
 
+         $cpaChart7 = $this->cpaChart($user_id, null, $offer_id, 7);
+         $cpaChart30= $this->cpaChart($user_id, null, $offer_id, 30);
+         $cpaChart90 = $this->cpaChart($user_id, null, $offer_id, 90);<div class="row">
+            <div class="col-md-12">
+               <div class="card ">
+                  <div class="card-header ">
+                     <h5 class="card-title">[cpa]</h5>
+                  </div>
+                  <div class="card-body ">
+
+                     <!-- Nav tabs -->
+                     <ul class="nav nav-tabs" role="tablist">
+                        <li class="nav-item">
+                           <a class="nav-link active" data-toggle="tab" href="#cpa7">7 Days</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" data-toggle="tab" href="#cpa30">30 Days</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" data-toggle="tab" href="#cpa90">90 Days</a>
+                        </li>
+                     </ul>
+
+                     <!-- Tab panes -->
+                     <div class="tab-content">
+                        <div id="cpa7" class="container tab-pane active"><br>
+                           <h5>7 Days Profit</h5>
+                           {!! $cpaChart7->render() !!}
+
+
+                        </div>
+                        <div id="cpa30" class="container tab-pane fade"><br>
+                           <h5>30 Days Profit</h5>
+                           {!! $cpaChart30->render() !!}
+
+                        </div>
+                        <div id="cpa90" class="container tab-pane fade"><br>
+                           <h5>90 Days Profit</h5>
+                           {!! $cpaChart90->render() !!}
+
+
+                        </div>
+                     </div>
+
+
+                  </div>
+               </div>
+            </div>
+         </div>
+
          @if(Auth::user()->is_monetize == false)
 <div class="row">
          <div class="col-md-12">
