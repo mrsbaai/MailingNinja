8a34b12ef906238d29b79a9b492de45a1a6bf6ae
mrsbaai <abdelilah.sbaai@gmail.com>
2019-04-10 21:34:42 +0100

    cpc to dashboard

--- resources/views/publisher/home.blade.php
+++ resources/views/publisher/home.blade.php
@@ -31,6 +31,7 @@
                 </div>
             </div>
             @endif
+                @if(Auth::user()->is_monetize == true)
             <div class="col-md-6 col-sm-6">
                 <div class="card card-stats">
                     <div class="card-body ">
@@ -57,34 +58,8 @@
                     </div>
                 </div>
             </div>
-                @if(Auth::user()->is_monetize == false)
-            <div class="col-md-6 col-sm-6">
-                <div class="card card-stats">
-                    <div class="card-body ">
-                        <div class="row">
-                            <div class="col-5 col-md-4">
-                                <div class="icon-big text-center icon-warning">
-                                    <i class="nc-icon nc-shop text-warning"></i>
-                                </div>
-                            </div>
-                            <div class="col-7 col-md-8">
-                                <div class="numbers">
-                                    <p class="card-category">Leads <br/>(Today)</p>
-                                    <p class="card-title">{{$data['leads_today']}}
-                                    <p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="card-footer ">
-                        <hr>
-                        <div class="stats">
-                            <i class="fa fa-refresh"></i> {{$data['leads_all']}} Lifetime
-                        </div>
-                    </div>
-                </div>
-            </div>
                 @endif
+
 
             <div class="col-md-6 col-sm-6">
                 <div class="card card-stats">
