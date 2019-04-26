3f86243ecb74633f339378fd9914454cad6e1522
mrsbaai <abdelilah.sbaai@gmail.com>
2019-04-26 15:10:48 +0100

    activate

--- app/Http/Controllers/managerController.php
+++ app/Http/Controllers/managerController.php
@@ -360,7 +360,7 @@ class managerController extends Controller
             ->isCustomHtmlElement(function ($entity, $column) {
 
                 $activate_route = route('global-offer-activate', ['offer_id' => $entity->id]);
-                return  "<a class='p-3' target='blank' href='$activate_route'  title='Activate'><h2>Activate</h2></a>";
+                return  "<a class='p-3'  href='$activate_route'  title='Activate'><h2>Activate</h2></a>";
             });
         $table2->addColumn()
             ->setTitle(__(' '))
