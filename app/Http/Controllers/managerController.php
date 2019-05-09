1e51c110ce279316bb793e234f8c05a76eef17bb
mrsbaai <abdelilah.sbaai@gmail.com>
2019-05-09 18:46:52 +0000

    offer countries

--- app/Http/Controllers/managerController.php
+++ app/Http/Controllers/managerController.php
@@ -24,7 +24,7 @@ class managerController extends Controller
     public function test(){
 
         $codes = country::whereIn('code', array("US","MA"))->get()->pluck('id')->toarray();
-return $codes;
+
         $offer = offer::find(3);
 
         $offer->countries()->sync($codes);
