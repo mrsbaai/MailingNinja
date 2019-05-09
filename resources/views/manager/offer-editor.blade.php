8fd144bca33300fcd8da7255b40e748bfbb83943
mrsbaai <abdelilah.sbaai@gmail.com>
2019-05-09 19:04:55 +0000

    select countries

--- resources/views/manager/offer-editor.blade.php
+++ resources/views/manager/offer-editor.blade.php
@@ -535,7 +535,9 @@
 
 @section('footer')
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.6/css/mdb.min.css" />
 
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.7.6/js/mdb.min.js"></script>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script type="text/javascript" src="{{ URL::asset('js/core/popper.min.js') }}"></script>
